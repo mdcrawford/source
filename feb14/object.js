6b2c435135a5744788cec98785041389815bd03a
Michael Crawford <michael@Michaels-MacBook-Pro.local>
2018-02-14 16:20:39 -0500

    minor change

--- feb14/object.js
+++ feb14/object.js
@@ -11,3 +11,6 @@ let person = {
 console.log(person.age); // 21
 console.log(person.family.mother); // "Kate"
 console.log(person.family); // ???
+
+person.year = 4;
+console.log(person); // note the object has a new field

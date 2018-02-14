eb74e534ba20b0b02a6ae43f2b3221fe97c1316b
Michael Crawford <michael@Michaels-MacBook-Pro.local>
2018-02-14 15:21:15 -0500

    object and JSON examples

---
+++ feb14/object.js
@@ -0,0 +1,13 @@
+let person = {
+  name: "Michael Crawford",
+  age: 21,
+  uvaStudent: true,
+  family: {
+    mother: "Kate",
+    father: "David"
+  }
+};
+
+console.log(person.age); // 21
+console.log(person.family.mother); // "Kate"
+console.log(person.family); // ???
